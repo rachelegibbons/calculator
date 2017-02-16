6ebc2125fb69ede06fe6265f68b203723d9fd9ca
Rachel Gibbons and Susha Dore <susha@>
2017-02-15 18:32:11 -0800

    add front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,28 @@
+//Business Logic (Back-End) Logic:
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1,number2) {
+  return number1 - number2;
+};
+
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert("Your number is " + multiply(number1,number2));
-
-
-var add = function(number1, number2) {
-  return number1 + number2;
-}
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert("Your number is " + add(number1,number2));
-
-
-var subtract = function(number1,number2) {
-  return number1 - number2;
-}
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-
-alert("Your number is " + subtract(number1,number2));
-
-
 var divide = function(number1, number2) {
   return number1 / number2;
-}
+};
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+//User Interface (Front-End) Logic:
 
-alert("Your number is " + divide(number1,number2));
+$(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
